f3fb60c3519f655fb3d36c288267ec74b349719b
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2024-10-25 07:53:18 +0200

    lagt till ~ tecken i alla rutor på spelplanen som representerar vatten

--- BattleshipGame/Program.cs
+++ BattleshipGame/Program.cs
@@ -12,6 +12,15 @@ namespace Battleship
             //skapar ett 10x10 rutnät
             char[,] grid = new char[10, 10];
 
+            //fyller spelplanen med symboler som representerar vatten
+            for (int row = 0; row < 10; row++)
+            {
+                for (int col = 0; col < 10; col++)
+                {
+                    grid[row,col] = '~'; //representerar vågor
+                }
+            }
+
             //Anropar metoden PrintGrid för att visa rutnätet i konsollen
             PrintGrid(grid);          
 
@@ -20,7 +29,7 @@ namespace Battleship
         //Metod för att skriva ut rutnätet till konsollen
         static void PrintGrid(char[,] grid)
         {
-            Console.WriteLine(" 0 1 2 3 4 5 6 7 8 9 "); //rubriker till kolumnerna
+            Console.WriteLine("  0 1 2 3 4 5 6 7 8 9 "); //rubriker till kolumnerna
             for (int row = 0; row < 10; row++)
             {
                 Console.Write(row + " "); //radnumrering
