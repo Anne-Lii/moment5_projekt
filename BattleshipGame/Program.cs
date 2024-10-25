414c9914b9f7edf0bca1f453b555614e583f54f0
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2024-10-25 07:49:36 +0200

    lagt till rader och kolumner till spelplan

--- BattleshipGame/Program.cs
+++ BattleshipGame/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Anne-Lii Hansen
+Spelet "Sänka skepp" eller "Battleship" med C#.NET */
+
+using System;
+
+namespace Battleship
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //skapar ett 10x10 rutnät
+            char[,] grid = new char[10, 10];
+
+            //Anropar metoden PrintGrid för att visa rutnätet i konsollen
+            PrintGrid(grid);          
+
+        }
+
+        //Metod för att skriva ut rutnätet till konsollen
+        static void PrintGrid(char[,] grid)
+        {
+            Console.WriteLine(" 0 1 2 3 4 5 6 7 8 9 "); //rubriker till kolumnerna
+            for (int row = 0; row < 10; row++)
+            {
+                Console.Write(row + " "); //radnumrering
+                for (int col = 0; col < 10; col++)
+                {
+                    Console.Write(grid[row, col] + " ");
+                }
+                Console.WriteLine();//tom rad
+            }
+        }
+    }
+}
+
